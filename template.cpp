1b6531105bbd4380abda37811225eb4fa8d08869
Tirth Motka <74975220+SuperStar0907@users.noreply.github.com>
2021-10-04 01:14:54 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,53 @@
+#include "bits/stdc++.h" /* < > in place of " " also works but for macos users who need to make a directory named bits and put the file stdc++.h in the current working directory they need " " */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+using namespace std; 
+
+#define ll long long
+#define rep(i, b) for (ll i = 0; i < b; i++)
+#define fr(i ,a, b) for (ll i = a; i < b; i++)
+#define rev(i, b) for (ll i = b-1; i >=0 ; i--)
+#define in(a, n) rep(i, n) cin >> a[i];
+#define outp(a) cout << a << endl;
+#define out(a, n) rep(i, n) cout << a[i] << " ";
+#define MIN -1e9
+#define MAX 1e9
+#define PI acos(-1)
+#define inp(a) ll a; cin>>a;
+#define endl "\n"
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define vl vector<ll>
+#define ff first
+#define ss second
+#define sz(x) ((ll)(x).size())
+#define boo(x) ((x==0)?cout << "NO" << endl; : cout << "YES" << endl;)
+
+typedef pair<int,int> pii;
+typedef vector<pii> vii;
+typedef vector<int> vi;
+typedef pair<ll,ll> pll;
+typedef vector<pll> vll;
+typedef vector<vi> vvi;
+typedef unsigned long long ull;
+typedef long double lld;
+/*keep third $ of snippet here*/
+void solve(){
+  /*keep second $ of snippet here*/
+  return;
+}
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+#endif
+  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  ll tt = 1;
+  /*keep first $ of snippet here*/
+  cin >> tt;
+  while(tt--)
+    solve();
+  return 0;
+}
